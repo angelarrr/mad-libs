f85a1c2a1555eb22f11eb637eb6f97bf0720951a
angelarrr <kwon.angela@gmail.com>
2015-07-09 21:31:19 -0400

    give users ability to pick gender

--- js/madLibs.js
+++ js/madLibs.js
@@ -9,4 +9,17 @@ angular.module('madLibs', [])
 		$scope.adjective = "adjective";
 		$scope.obnoxiousCelebrity = "obnoxious celebrity";
 		$scope.hugeNumber = "enter a huge number";
+		$scope.gender = "";
+
+		$scope.male = function() {
+			$scope.heShe = "he";
+			$scope.himHer = "him";
+			$scope.hisHer = "his";
+		};
+
+		$scope.female = function() {
+			$scope.heShe = "she";
+			$scope.himHer = "her";
+			$scope.hisHer = "her";
+		}
 	});
