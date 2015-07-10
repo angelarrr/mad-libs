b1c0e80dbd67d7cbb2ddc819a2393defb652cd31
angelarrr <kwon.angela@gmail.com>
2015-07-09 20:51:34 -0400

    add controller

---
+++ js/madLibs.js
@@ -0,0 +1,12 @@
+angular.module('madLibs', [])
+	.controller('madController', function($scope) {
+		$scope.name = "name";
+		$scope.jobTitle = "job title";
+		$scope.tediousTask = "tedious task";
+		$scope.dirtyTask = "dirty task";
+		$scope.celebrity = "celebrity name";
+		$scope.uselessSkill = "useless skill";
+		$scope.adjective = "adjective";
+		$scope.obnoxiousCelebrity = "obnoxious celebrity";
+		$scope.hugeNumber = "enter a huge number";
+	});
