c2bcec072bbc5ad054531e3e964c2e8a5c003c4d
angelarrr <kwon.angela@gmail.com>
2015-07-15 14:05:35 -0400

    play around and add animation

--- js/madLibs.js
+++ js/madLibs.js
@@ -1,46 +1,47 @@
-angular.module('madLibs', [])
-	.controller('madController', function($scope) {
+var app = angular.module('madLibs', ['ngAnimate'])
+
+app.controller('madController', function($scope) {
+	$scope.display = 0;
+
+	$scope.male = function() {
+		$scope.heShe = "he";
+		$scope.himHer = "him";
+		$scope.hisHer = "his";
+	};
+
+	$scope.female = function() {
+		$scope.heShe = "she";
+		$scope.himHer = "her";
+		$scope.hisHer = "her";
+	};
+
+	$scope.submit = function(){
+		if ($scope.myForm.$valid) {
+			console.log('Form is valid!');
+			$scope.display = !0;
+		} else {
+			console.log('Form is not valid!');
+		}
+	};
+
+	$scope.reset = function() {
+		// reset display
 		$scope.display = 0;
 
-		$scope.male = function() {
-			$scope.heShe = "he";
-			$scope.himHer = "him";
-			$scope.hisHer = "his";
-		};
+		// reset form to unsubmitted state
+		$scope.myForm.$setPristine();
+		$scope.myForm.$setUntouched();
 
-		$scope.female = function() {
-			$scope.heShe = "she";
-			$scope.himHer = "her";
-			$scope.hisHer = "her";
-		};
-
-		$scope.submit = function(){
-			if ($scope.myForm.$valid) {
-				console.log('Form is valid!');
-				$scope.display = 1;
-			} else {
-				console.log('Form is not valid!');
-			}
-		};
-
-		$scope.reset = function() {
-			// reset display
-			$scope.display = 0;
-
-			// reset form to unsubmitted state
-			$scope.myForm.$setPristine();
-			$scope.myForm.$setUntouched();
-
-			$scope.name = "";
-			$scope.jobTitle = "";
-			$scope.tediousTask = "";
-			$scope.dirtyTask = "";
-			$scope.celebrity = "";
-			$scope.uselessSkill = "";
-			$scope.adjective = "";
-			$scope.obnoxiousCelebrity = "";
-			$scope.hugeNumber = "";
-			$scope.gender = "";
-		};
-				
-	});
+		$scope.name = "";
+		$scope.jobTitle = "";
+		$scope.tediousTask = "";
+		$scope.dirtyTask = "";
+		$scope.celebrity = "";
+		$scope.uselessSkill = "";
+		$scope.adjective = "";
+		$scope.obnoxiousCelebrity = "";
+		$scope.hugeNumber = "";
+		$scope.gender = "";
+	};
+			
+});
